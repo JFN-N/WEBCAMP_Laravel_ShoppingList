9dbc8a4ba55d15c463e7002a76fac5e753764bab
EC2 Default User <ec2-user@ip-172-31-34-4.ap-northeast-1.compute.internal>
2023-06-08 11:38:56 +0000

    任意のメッセージ

--- database/migrations/2023_06_07_144924_create_shopping_lists_table.php
+++ database/migrations/2023_06_07_144924_create_shopping_lists_table.php
@@ -16,6 +16,7 @@ class CreateShoppingListsTable extends Migration
         Schema::create('shopping_lists', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('「買うもの」名');
+            $table->unsignedBigInteger('user_id')->comment('任意のコメント');
             $table->foreign('user_id')->references('id')->on('users'); // 外部キー制約
             //$table->timestamps();
             $table->dateTime('created_at')->useCurrent();
