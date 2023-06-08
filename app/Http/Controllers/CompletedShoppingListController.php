81624e17e8721fdcbb716a412bafbc4e686bf156
EC2 Default User <ec2-user@ip-172-31-34-4.ap-northeast-1.compute.internal>
2023-06-08 13:13:12 +0000

    任意のメッセージ

---
+++ app/Http/Controllers/CompletedShoppingListController.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class CompletedShoppingListController extends Controller
+{
+    public function index()
+    {
+        return view('/completed_shopping_list/list');
+    }
+}
