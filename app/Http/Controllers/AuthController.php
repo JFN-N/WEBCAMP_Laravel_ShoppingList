c2a340b78c2b7e8a5e0e6818d89bf1fc7f4f84d8
EC2 Default User <ec2-user@ip-172-31-34-4.ap-northeast-1.compute.internal>
2023-06-10 12:41:02 +0000

    任意のメッセージ

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginPostRequest;
 
+use Illuminate\Support\Facades\Auth;
+
 class AuthController extends Controller
 {
     /**
