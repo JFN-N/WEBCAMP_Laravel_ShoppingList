5f8b413c2004c3014f3620c2de6e43f4cada5fcd
EC2 Default User <ec2-user@ip-172-31-34-4.ap-northeast-1.compute.internal>
2023-06-11 14:47:51 +0000

    任意のメッセージ

---
+++ resources/views/admin/user/list.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layout')
+
+{{-- メインコンテンツ --}}
+@section('admin.contets')
+    <a href="./index.html">管理画面Top</a><br>
+    <a href="./index.html">ユーザー一覧</a><br>
+    <a href="./index.html">ログアウト</a><br>
+
+    <h1>ユーザー一覧</h1>
+
+    <table border="1">
+        <tr>
+            <th>ユーザーID
+            <th>ユーザー名
+            <th>購入した「買うもの」数
+        </tr>
+    </table>
+
+@endsection
