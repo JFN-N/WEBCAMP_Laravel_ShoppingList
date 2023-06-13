58202a42d9fa126b57b9f023213e7081037c5ed0
EC2 Default User <ec2-user@ip-172-31-34-4.ap-northeast-1.compute.internal>
2023-06-13 13:00:51 +0000

    任意のメッセージ

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User as UserModel;
+
+class UserController extends Controller
+{
+    /**
+     * ユーザの一覧 を表示する
+     *
+     * @return \Illuminate\View\View
+     */
+    public function list()
+    {
+        $group_by_column = ['users.id', 'updated_date'];
+        $list = UserModel::select($group_by_column)
+                         ->selectRaw('count(tasks.id) AS task_num')
+                         ->leftJoin('tasks', 'users.id', '=', 'tasks.user_id')
+                         ->groupBy($group_by_column)
+                         ->orderBy('users.id')
+                         ->get();
+
+        return view('admin.user.list', ['users' => $list]);
+    }
+
+}
