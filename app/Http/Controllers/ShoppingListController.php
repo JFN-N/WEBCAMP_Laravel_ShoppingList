c4ebeea44c1c59dba92bb7e7c340c70a84442ed8
EC2 Default User <ec2-user@ip-172-31-34-4.ap-northeast-1.compute.internal>
2023-06-17 12:32:58 +0000

    任意のメッセージ

--- app/Http/Controllers/ShoppingListController.php
+++ app/Http/Controllers/ShoppingListController.php
@@ -117,7 +117,7 @@ class ShoppingListController extends Controller
 
             // tasks側を削除する
             $Shopping_lists->delete();
-//var_dump($task->toArray()); exit;
+var_dump($Shopping_lists->toArray()); exit;
 
             // completed_tasks側にinsertする
             $dask_datum = $Shopping_lists->toArray();
@@ -128,14 +128,16 @@ class ShoppingListController extends Controller
                 // insertで失敗したのでトランザクション終了
                 throw new \Exception('');
             }
-//echo '処理成功'; exit;
+echo '処理成功'; exit;
 
             // トランザクション終了
             DB::commit();
+
+
             // 完了メッセージ出力
             $request->session()->flash('front.task_completed_success', true);
         } catch(\Throwable $e) {
-//var_dump($e->getMessage()); exit;
+var_dump($e->getMessage()); exit;
             // トランザクション異常終了
             DB::rollBack();
             // 完了失敗メッセージ出力
