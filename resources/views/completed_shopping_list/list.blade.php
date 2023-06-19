fe5f3575301321ed5a75501655336027e2fc6303
EC2 Default User <ec2-user@ip-172-31-34-4.ap-northeast-1.compute.internal>
2023-06-19 11:03:18 +0000

    任意のメッセージ

--- resources/views/completed_shopping_list/list.blade.php
+++ resources/views/completed_shopping_list/list.blade.php
@@ -2,18 +2,41 @@
 
 @section('contets2')
 
-        <h1>購入済み「買うもの」一覧</h1>
-
-
+        <h1>完了タスクの一覧</h1>
+        <a href="/task/list">タスク一覧に戻る</a><br>
+        <table border="1">
+        <tr>
+            <th>「買うもの」名
+            <th>購入日
+@foreach ($list as $task)
+        <tr>
+            <td>{{ $task->name }}
+            <td>{{ $task->created_at->format('Y/m/d')}}
+@endforeach
+        </table>
         <!-- ページネーション -->
-        現在 1 ページ目<br>
-        <a href="./top.html">最初のページ</a> /
-        <a href="./top.html">前に戻る</a> /
-        <a href="./top.html">次に進む</a>
+        {{-- {{ $list->links() }} --}}
+        現在 {{ $list->currentPage() }} ページ目<br>
+        @if ($list->onFirstPage() === false)
+            <a href="/completed_shopping_list/list">最初のページ</a>
+        @else
+            最初のページ
+        @endif
+        /
+        @if ($list->previousPageUrl() !== null)
+            <a href="{{ $list->previousPageUrl() }}">前に戻る</a>
+        @else
+            前に戻る
+        @endif
+        /
+        @if ($list->nextPageUrl() !== null)
+            <a href="{{ $list->nextPageUrl() }}">次に進む</a>
+        @else
+            次に進む
+        @endif
         <br>
         <hr>
-        <menu label="リンク">
-        <a href="./index.html">ログアウト</a><br>
+        <a href="/logout">ログアウト</a><br>
         </menu>
 
 @endsection
